79301a18b2a528897d20fb8513a930558ae1a714
Alexander Winkler <alexander.w.winkler@gmail.com>
2019-04-01 17:54:17 +0200

    start polishing travel post

--- posts/six-months.js
+++ posts/six-months.js
@@ -7,7 +7,7 @@ module.exports = {
   content: 
   `
 <p>
-<span class="first-letter">S</span>ix months of backpacking is over, and here I want to retain some of the things I've learned.ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+<span class="first-letter">S</span>ix months of backpacking is over, and here's a brief summary for myself and others before I forget^^.
 </p>
 
 
@@ -16,19 +16,15 @@ module.exports = {
   <div class="card-body">
 	  <h5 class="card-title">Numbers</h5>
 		<ul>
-			<li>Days: 181</li>
-			<li>Countries: Thailand (30), Cambodia (10), Vietnam (30), USA (30), Mexico (30), Indonesia (60), Philipines (20)</li>
-			<li>Number of flights (sorry environment): </li>
-			<li>Money spent in each country: 75$/day</li>
-			<li>Backpack: 40L, 9kg</li>
-			<li>Underpants: 2</li>
-			<li>Kilometers travelled without flights:</li>
-			<li>Hostels/Hotels: </li>
-			<li>Days with Diarrea: 10</li>
-			<li>GoJek rides:</li>
+			<li>Days: 181 (15.9.2018 - 15.3.2019) </li>
+			<li>Countries: Thailand (26), Cambodia (7), Vietnam (30), USA (15), Mexico (25), Indonesia (59), Philipines (19)</li>
+			<li>Money spent per day (no flights): 75$</li>
+			<li>Backpack: Osprey Farpoint (40L, 9kg)</li>
+			<li>Hostels: 40</li>
 			<li>Most favorite backpacking country: Vietnam</li>
-			<li>Friend visits from home:</li>
-			<li>Max continuous days without friend from home: (70)</li>
+			<li>Longest stay in one place: Canggu, Bali, 45 days.</li>
+			<li>Underpants: 2</li>
+			<li>Days with Diarrea: 10</li>
 		</ul>
 </div>
 </div>
@@ -36,168 +32,96 @@ module.exports = {
 
 <div class="card bg-light mb-3">
 <div class="card-body">
+	<h5>Most memorable Experiences</h5>
+	<h6 class="subheader">
+		...made 10x more fun by experiencing it with amazing old and new friends.
+	</h6>
+	<ul>
+		<li>riding Nightbuses and Nighttrains (TH, VN)</li>
+		<li>10-day silent meditation camp (VN)</li>
+		<li>eating Chicken Feet (VN)</li> 
+		<li>Motorbiking the Hai Van Pass (VN)</li> 
+		<li>Paradise Cave in Phong Nha (VN)</li>
+		<li>swimming with Bioluminescene (MX)</li>
+		<li>eating Tacos al Pastor in Mexico City</li>
+		<li>Scuba diving through a fish swarm in Amed, Bali.</li>
+		<li>surfing waves in Bali & Siargao</li>
+	</ul>
+
+
 	<h5>Skills improved</h5>
 	<ul class="list-horizontal">
-		<li>Sleep in room with strangers</li>
+		<li>sleeping in dorms</li>
 		<li>eating with chopsticks</li>
 		<li>using Google Maps for life</li>
 		<li>googling VISA information</li>
 		<li>accurately judging a hostel through Hostelworld images</li>
 		<li>finding ATMs</li>
-		<li>Photography</li>
+		<li>photography</li>
 		<li>expertly using "Bum-Guns"</li>
-		<li>driving a scooter</li>
+		<li>how to kick-start a scooter</li>
 		<li>Vipasanna meditation</li>
 		<li>some History (Khmer Rouge, Vietnam War)</li>
-		<li>Prepare/Teaching an english lesson</li>
+		<li>teaching english (workaway.info)</li>
 		<li>cross a street in Ho Chi Minh City without dying</li>
-		<li>PADI Open Water
 	  <li>Jenga</li>
 	  <li>Marco-Polo</li>   
 		<li>surfing</li>
 		<li>yoga</li>
-		<li>small-talk with strangers</li>
+		<li>scuba diving</li>
+		<li>small-talk</li>
 		<li>Web Development (html, css, js, nodejs)</li>
 		<li>Game Development (Unity)</li>
-		<li>Axe-throwing</li>
-		<li>Rocket-League</li>
-	</ul>    
+	</ul> 
 
-	<h5>Novel Experiences</h5> 
-	<ul>
-		<li>see Bioluminescene (MX)</li>
-		<li>riding Nightbuses and Nighttrains (Thailand, VN)</li>
-		<li>ate Crickets (Cambodia)</li>
-		<li>Durian (VN)</li>
-		<li>Chicken Feet (VN)</li> 
-		<li>Saw how many ppl can fit on one scooter</li>
-		<li>Motorbiked the Hai Van Pass (VN)</li> 
-		<li>huge Caves in Phong Nha (VN)</li>
-		<li>ate amazing Tacos al Pastor (Mexico City)</li>
-	</ul>
 </div>
 </div>
 
 
 
 
-
-
-
-
-
-<h4>Most valuable lessons learned?</h4>
+<h4>Some lessons learned?</h4> 
 <p>
-
-I'm so privileged and have so many options. 
+I'm so privileged and have so many options. Nobody has cars here. 
 How difficult it can be for ppl with other passports and less money to travel.
 So many simultaneous lives going on at this instance arround the world.
-I'm often overprepared in terms of money, insurance, medial supply, fancy clothes (merione whool)... Many ppl did what I did with much less to start with, but still did it and managed. I can definetly take more risks. You don't have to plan a lot. I arrived in Saigon by Bus at night without money or internet and it still worked. Don't be so scared.
-</p>
-
-<p>
-Everyone is busy with their own life. People don't really think about me that often. This is not meant in a bitter, negative sense, but as encouragement to stop obsessing about what others think about me. You're on a large stage but the entire audience is just looking at their smartphone answering emails, because they also got shit to do, that' fine. I can do whatever I want, and just as     
 </p>
 
 
 <p>
-(Life is about other humans.)
-Life is useless. We make up arbitary things to keep us busy, but ultimately there's no reason why any of us <em>need</em> to live. The earth doesn't need humans to survive (probabaly on the contrary), and nobody misses a person who never existed. So since were already alive, what to do? It seems that human connection, sharing this useless existence with other people, is one of the only things matters. If I imagine a world in which I were the only human, I wouldn't be writing code or working on a company. Even the "professional" world is made mostly interesting not by the products, but by the fact the <em>other people</em> use our products, <em>other people</em> benefit from our product, we gain recognition from <em>other people</em> for our work, we compete and collaborate with <em>other people</em> to push us to be our best. We can tell ourselves we just love creating/building, but that's just part of the picture. Without other people, in an uninhabited world, there would be little satisfaction in developing all this alone in our basement.
-
-
-
-Types of interactions with other people in the professional world:
-- ppl are happier because of our we/our product helps them.
-- we gain recognition from other ppl for our work
-- we compete with other ppl
-- we collaborate with other ppl
-
-
-
-
-
-
-
-
-
-
-We can't convince people that there is an objective reason for them to live, but we can make their lives more bearable. That seems like the only sensible thing to do. 
-We can support this cycle of humans breeding more useless humans, and making their lives more bearable.    
-
-
-
-
-
-A human alone on earth would be suuuuuper boring, no reason to live! Would use animals as other sentient beings for comfort. If also no animals, only nature, that would really be a sad existence. So Premise 1:
-Humans need sentient beings to connect with.
-
-
-
-
-
-
-
-
-ince there is no objective goal for any of us, the only thing we can focuss on is the subjective experience of each  
-
-
-
-Since we're all playing this game without a goal together, the only sensible thing      
-
-
-
-
-
- The only thing we can observe is the happiness of the people along the way (to death).     
-
-
-
-
-
-Improving the life of one human being of 7 600 000 000, seems like a useless/unimportant task, who cares?! That means it doesn't make sense to dedicate my life to my career, my happiness, or making money - I'm just one human. The same is true for me dedicating my life to helping <em>one</em> other person -- it equally makes no difference. What about helping 1..2..20 other people? Where's that magic number after which your life has been worth living? At this stage of my ignorance, i'll say, the more the better. But one thing that's for sure, is just focussing on myself will forever keep that number at 1, and I know I can do better than that.  
-
-
-
-Something that would seem like a    
-
-Usefulness instead of Happiness:
-https://medium.com/darius-foroux/the-purpose-of-life-is-not-happiness-its-usefulness-65064d0cdd59
-
-
-Maslow's pyramid of needs
-
-</p>
-
-
-
-
-Everyone is busy with their own life. Not in a negative sense, but people don't really care what you do or think about you that often. That's also very freeing, because you start to understand you are alone and also can do whatever you want, nobody's really watching. And noone you really have to impress, because everyone thinks about themselves. Time goes on and some people do this, others that. There isn't THE optimal lifestyle, everyone has its own. For me I feel oppressed living in a small town in germany. I feel best when I'm among young people. I feel best when I'm in warm weather.
+Everyone is busy with their own life. People don't really think about you that often. This is not meant in a bitter, negative sense, but as encouragement to stop obsessing about trying to please others. You're on a large stage but the entire audience is just looking at their smartphone answering emails, because they also got shit to do, that's fine. 
+You can do whatever I want, just as other people are going their way, thinking about their own problems. This includes your parents and friends, they'll love and support you, but ultimately this is a path you walk alone, so make your own decisions.
 </p>
 
 
 <p>
-Whereas previously 70% of my conversation involved robotics, now only the occasional facebook post reminded me of my profession. And seeing a new robotics paper now seems a bit less...important. I'm still very interested in the scientific aspects, but academic research is so far removed from life of the non-researcher. In the travel-bubble (also very much a bubble), people mostly talk about the places they visited, the things they experienced, the people they met. My citations or journals don't interest anyone in this environment. A post from my Zurich flatmate Stephanie, after she finished her PhD, resonated with me in which she said: <em>"Might be done accumulating qualifications now"</em>. Enough of accumulating citations, papers and job-experience to prove that I know what I'm doing. Let's start and actually do and build something, something scientifically challenging, but useful also outside the bubble of academia. (also goes into Usefullnes direction).
+Whereas previously 70% of my conversation involved robotics, now only the occasional facebook post reminded me of my profession. And seeing a new robotics paper now seems a bit less...important. I'm still very interested in the scientific aspects, but academic research is so far removed from life of the non-researcher. In the travel-bubble (also very much a bubble), people mostly talk about the places they visited, the things they experienced, the people they met. My citations or journals don't interest anyone in this environment. 
 </p>
 
 
 <p>
-I smoked occasionally at the beginning of the travel, as many people in the backpacking community do. I understood that my main reason for smoking was that I enjoyed being able to stand somewhere doing nothing without looking stupid. It was an introvert thing. So I was mostly missing the self-confidence to just idly stand at the edge of a party. Instead of smoking I will work on my self-confidence. 
-
-I hate hangovers. That's why I rarely drank more than a few beer. But if 8 beer cause a terrible hangover, indicating that my body is harmed by alcohol, then one beer still harms my body 1/8 of the terrible hangover. Even if I don't feel it. I'll only accept this health-hazard, if the alcohol I'm drinking is delicious and there's no non-alcoholic alternative. Therefore I'm forever done with shots (juck!) and beer and whine are replaced by tasty fresh-juice. Still looking for an alternativee to a a delicious mojito or gin&tonic ;) 
+I smoked occasionally at the beginning of the travel, as many people in the backpacking community do. I understood that my main reason for smoking was that I enjoyed being able to stand somewhere doing nothing without looking stupid. It was an introvert thing. So I was mostly missing the self-confidence to just idly stand at the edge of a party. Instead of smoking I will work on my self-confidence. I hate hangovers. That's why I rarely drank more than a few beer. But if 8 beer cause a terrible hangover, indicating that my body is harmed by alcohol, then one beer still harms my body 1/8 of the terrible hangover. Even if I don't feel it. I'll only accept this health-hazard, if the alcohol I'm drinking is delicious and there's no non-alcoholic alternative. Therefore I'm forever done with shots (juck!) and beer and whine are replaced by tasty fresh-juice. Still looking for an alternativee to a a delicious mojito or gin&tonic ;) 
 </p>
 
-<!-- <p>
-Travelling isn't neccessary to make you a better, more world-open person. I met a few interesting people, who just started their first travels a few days ago and impressed me in the genuonly curious way they engaged with the country, the people and the mindset they had. This trait was something they picked up at home, and they can be just as good an embassador for their country than the long-term traveler. In the same way, just because the natives you meet haven't travelled to other countries, really doesn't make them worse people in any way. 
-</p>  -->
+<p>
+How lively the streets are (also a night) and how much more people are socializing compared to "developed countries". Money brings isolation? Really noticed in the states how empty streets are, it's car to work, to house.
+</p>
+
+<p>
+How many people are currently travelling or working remotely. That that's a life one can have and choose.
+</p>
 
 
 
-<h4>Something I changed my mind about through the 6 months travel?</h4>
+<h4>Some other things I learned</h4>
 <ul>
-	<li>Partying is really not fun for me (anymore).</li>
-	<li>America was bad in Vietnam war.</li>
-	<li>Plastic waste is a top issue in Indonesia/Philipines. Paddled out through dirty water in Canggu.</li>
+	<li>America was the bad guy in Vietnam war.</li>
+	<li>Plastic waste is a real issue in Bali. Paddled out through dirty water in Canggu.</li>
 	<li>Top bunk bed is not better than bottom bed.</li>
+	<li>Sharing a room with other people does not reduce happiness.</li>
+	<li>Partying is really not fun for me anymore.</li>
+	<li>Saw that 4 males can fit on one scooter</li>
+	<li>SE-Asia is extremely safe. Not one criminal incident in 6 months. Also never used a locker in the dorms (just a lock on my bag) and never had anything stolen.  
 </ul>
 
 
@@ -217,27 +141,9 @@ Not so much for me. Sightseeing/exploring I prefer to do with other people. Goin
 </p>
 
 
-
-<h4>Some inspring people I've come in contact with</h4>
-<p>
-Woman breaking off from Westborow Baptist Church, UK woman leaving home for NZ, Programmer working abroad, Sinag Hostel women starting a business (unprepared?)
-</p> 
-
-
-
-<h4>Random things that I stuck</h4>
-<ul>
-	<li>My Vietnamese roommate Huan explaining how to use a "Bum Gun".</li>
-	<li>How lively the streets are (also a night) and how much more people are socializing compared to "developed countries". Money brings isolation?</li>
-	<li>How helpful/efficient people where in Amed Bali and fixed our flat tire.</li>
-</ul>
-
-
-
-
 <h4>What will I do differently next time?</h4>
 <p>
-I noticed that 6 months of pure travelling/sightseeing is quite long. 3 weeks every 6 months sight-seeing vacations are enough. Pick one country and go all-in, hostel, social, tours. The other time stay in a place with routine, and work efficiently on something (optimally this is my job) and do sports, e.g. surfing. Because pure travel can be exhausting and conversely, working efficiently and doing sports is difficult on the road. 
+I noticed that six months of pure travelling/sightseeing is quite long. 3 weeks every 6 months sight-seeing vacations are enough. Pick one country and go all-in, hostel, social, tours. The other time stay in a place with routine, and work efficiently on something (optimally this is my job) and do sports, e.g. surfing. Because pure travel can be exhausting and conversely, working efficiently and doing sports is difficult on the road. 
 </p>  
 
 
@@ -245,7 +151,7 @@ I noticed that 6 months of pure travelling/sightseeing is quite long. 3 weeks ev
   <div class="card-body">
   <h5 class="card-title">Practical advice</h5>
 	<p>
-	Technology makes travelling sooo easy nowadays. The first thing when you arrive at a country/airport is get Cash and a Sim Card with Internet (about 20$ for 15GB).
+	Technology makes travelling sooo easy nowadays. The first thing when you arrive at a country/airport is get Cash and a Sim Card with Internet (about 20$ for 15GB LTE).
 	</p> 
 
 	<h6>My most helpful apps</h6>
@@ -262,17 +168,6 @@ I noticed that 6 months of pure travelling/sightseeing is quite long. 3 weeks ev
 
 </div>
 </div>
-
-
-
-
-
-<h4>Most adventurous/scared</h4>
-<p>
-Nothing at all happened (robbed, stolen).
-Walking up to the top of the hill in Ninh Binh in the morning.
-Scared when North Thailand guy came to bonfire, but as soon as he smiled, everything was okey :)
-</p>
 
 
 
