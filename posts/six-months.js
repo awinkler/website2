9aee828857bec68623b9d6ccd30eb738bffee990
Alexander Winkler <alexander.w.winkler@gmail.com>
2019-04-03 10:44:26 +0200

    finished post

--- posts/six-months.js
+++ posts/six-months.js
@@ -7,7 +7,7 @@ module.exports = {
   content: 
   `
 <p>
-<span class="first-letter">S</span>ix months of backpacking is over, and here's a brief summary for myself and others before I forget^^.
+<span class="first-letter">S</span>ix months of backpacking is over, and in this post I'm trying to retain some of the new reoccuring thoughts, insights or just curious moments during these travels. It turned out to be a super random collection, so don't be looking for the goldden thread, an agenda, a goal, just enjoy the journey ;)
 </p>
 
 
@@ -123,87 +123,95 @@ module.exports = {
 
 
 
-
-<h4>Some lessons learned?</h4>
-<h6>I'm so privileged</h6>
+<h4>Being grateful</h4>
 <p>
-This first one is so cliche, I considered leaving it out, but I think it's one of my most important realizations. How effortlessly I could save enough money to fly and experience a foreign country, compared to how much more difficult it would be for the college graduate from Phnom Phen to backpack through Europe. And I'm not just privileged in terms of money, but also options. Merely because I was born at the specific patch of earth to get a German Passport, I'm able to enter so many countries with so little bureucratic effort. I didn't earn this, do anything for it, that's also just given to me. 
-
-On top of money and open-doors to foreign countries, another privilege is environment. Although an education at a reknowned university doesn't guarantee professional success, it does increase the chances (if only connecting you with influencial people or convince company recruiters who put a disproportionate value on <em>where</em> you got your degree from). I have no doubt that there are equally capable kids as me in the schools in Cambodia or Vietnam, but if I imagine the challenges they would have to overcome to e.g. study at ETH Zurich: One of the hardest things is to even recognize that as a possibility, because you must do something so radically different than your parents, peers, environment. Then, they have to convince universities of their potential with a foreign report card in a system most recruiters can't accurately judge. On top of that, money and Visa will be huge issues. 
-</p>
-
-
-<p>
-Everyone is busy with their own life. People don't really think about you that often. I don't say this in a bitter, negative sense, quite the contrary. It takes the pressure off, like talking on a conference call, but the entire audience is just answering emails on their smartphone, because they also got shit to do and that's fine. Do whatever you want. At the same time you alone must take full responsiblity for your decisions. Don't make a decision that you'll be stuck with 100% of the time, based on the expectations, sentiments, beliefs of other people that might only be impacted by it 1% of the time. They don't have the skin in the game.
-</p>
-
-
-<p>
-Your environment strongly influences what you view as important. In the last 4 years I nodded in admiring recognition when someone got published in IJRR instead of "just" RA-L, found it self-evident to dedicate months of my life to optimize not just step-timings, but the gait sequence, had hour-long discussions on the crucial importance of modelling the effects of leg motions on body accelerations. It was super exciting to work on these challenges, but I forget how much of a niche I'm really in. And sometimes you might overblow the effect your research matters to the world. Of course I stomped from the academic robotics bubble right into the backpacker-bubble instead, where dinner conversations revolved about countries visited, breathtaking places you gotta see, local food you must try and that you don't want to go back home. Seeing what people view as important in this enviroment, combined with my previous bubbles, possibly gave me a more holistic perspective. In any case, there are many more bubbles to sample.  
-</p>
-
-
-<p>
-I smoked occasionally at the beginning of the travel, as many people in the backpacking community do. I understood that my main reason for smoking was that I enjoyed being able to stand somewhere doing nothing without looking stupid. It was an introvert thing. So I was mostly missing the self-confidence to just idly stand at the edge of a party. Instead of smoking I will work on my self-confidence. I hate hangovers. That's why I rarely drank more than a few beer. But if 8 beer cause a terrible hangover, indicating that my body is harmed by alcohol, then one beer still harms my body 1/8 of the terrible hangover. Even if I don't feel it. I'll only accept this health-hazard, if the alcohol I'm drinking is delicious and there's no non-alcoholic alternative. Therefore I'm forever done with shots (juck!) and beer and whine are replaced by tasty fresh-juice. Still looking for an alternativee to a a delicious mojito or gin&tonic ;) 
+This first one is so cliche, I considered leaving it out, but I think it's one of my most important realizations. How effortlessly I could save enough money to fly and experience a foreign country, compared to how much more difficult it would be vice-versa for the college graduate from Cambodia to backpack through Europe. And I'm not just privileged in terms of money, but also options. Merely because I was born at the specific patch of earth to get a German Passport, I'm able to enter so many countries with so little bureucratic effort. I didn't earn this, do anything for it, that's just given to me.
 </p>
 
 <p>
-How lively the streets are (also a night) and how much more people are socializing compared to "developed countries". Money brings isolation? Really noticed in the states how empty streets are, it's car to work, to house.
+The final privilege is environment. An education at a reknowned university doesn't guarantee professional success, but does increase the chances (even if only connecting you with influencial people or convince recruiters who put a disproportionate value on <em>where</em> you got your degree from). I have no doubt that there are equally capable kids as me in the schools in Cambodia or Vietnam, but imagine the challenges they would have to overcome to e.g. study at ETH. One of the hardest things is to even recognize that as a possibility, because they must do something so radically different than their parents, peers, environment. Then, they have to convince universities of their potential with a foreign report card recruiters can't accurately judge. On top of that, money and Visa will be huge issues. 
 </p>
+
+
+<h4>Everyone is busy with their own shit</h4>
+<p>
+Everyone is busy with their own life. People don't really think about you that often. I don't say this in a bitter, negative sense, quite the contrary. Realizing that the people in your life are independent humans with their own worries and agendas, mostly not concerning you, takes the pressure off. Do whatever <em>you</em> want and then take full responsiblity for your decisions. Don't make a decision that you'll be stuck with 100% of the time, based on the expectations, sentiments, beliefs of other people that might only be impacted by it 1% of the time. They don't have the skin in the game. And for times you are together with them, you can be best friend, brother, son if you are happy with your own life.
+</p>
+
+
+<h4>Being aware of my bubbles</h4>
+<p>
+Your environment strongly influences what you view as important. In the last 4 years I nodded in admiring recognition when someone got published in IJRR instead of <em>just</em> RA-L, found it self-evident to dedicate months of my life to optimize not just step-timings, but the gait sequence, had hour-long discussions on the crucial importance of modelling the effects of leg motions on body accelerations. It was super exciting to work on these challenges, but I forgot how much of a niche I was really in. And sometimes you might overblow the amount your research matters to the world. Of course I stomped from the academic robotics bubble right into the backpacker-bubble instead, where dinner conversations revolved about countries visited, breathtaking places you gotta see, local food you must try and that you don't want to go back home. Seeing what people view as important in this enviroment, combined with my previous bubbles, maybe gave me a bit more of a holistic perspective.  
+</p>
+
+
+<h4>Prioritizing Heath</h4>
+<p>
+I smoked occasionally at the beginning of the travel, as many people in the backpacking community do. I realized that my main reason for smoking was that I enjoyed being able to stand somewhere doing nothing without looking stupid. It was an introvert thing. You're still connected to the people, you can watch them, you can think, but it's not awkward if everyone just silently pulls on that glowing stick. By working on my self-confidence, I can still be in these situations, but without a cigarette in my hand.
+</p>
+
+<p>
+I hate hangovers. That's why I rarely drank more than a few beer. But if 8 beer cause a terrible hangover, signaling clearly that something in my body is seriously destroyed by the alcohol, then one beer still harms my body 1/8 of the amount &ndash; even if I don't feel it. I'll only accept this health-hazard, if the alcohol I'm drinking is delicious and there's no non-alcoholic alternative. Therefore I'm forever done with shots (juck!) and beer and whine are mostly replaced by tasty fresh-juice. I doubt i'll find an alternativee to a delicious mojito though, yummy! 
+</p>
+
+<h4>They're doing it right now</h4>
+<p>
+I wrote in a <a href="/blog/why-im-travelling">previous post</a> about sampling others ways of living. Travelling gave me a glimpse into what feels like a "Secret(ish) Society". These are backpackers, volunteers, online-teachers, writers, influencers, digital nomads, spread everywhere throughout the world, choosing to just live for a few weeks, months or years a bit different than the rest. They have differenet perks and problems, but nonetheless prove that work, play, life can alternatively be structured and the lines between them blurred. For me it is inspring to realize that they're many more than I thought, and they're living like this <em>right now</em> (my Instragram feed proving it). 
+</p>
+
+
+
+<h4>Two modes of travelling</h4>
+<p>
+I noticed for me there are two modes of travelling (read "living"). The first mode is exploration. That's changing hostels every few days, doing tours, sightseeing. It's difficult to get a consistent sleeping routine, or stick to a workout schedule, since there's so much to do, see and everything is always changing. It's exhillerating! But it can also be exhausting. I limit these backpacking sections to 3 weeks, and in that time go all-in: Socialize, participate, explore. But I don't even try to get work done, or sleep long, that's not what I'm there for.
+</p>
+
+<p>
+The other mode is exploitation. It's about dedicating time to getting something done, aquiring a specific skill, working on your health (also relaxing). For these tasks it's ideal to stay in one place for multiple weeks, get into a routine, be strict with yourself. I did this in Bali where I spent 45 days surfing, trying to eat healthy and learning web development.
+</p>
+
+<p>
+Of course you can also mix these modes, but it's helpful to be clear in one's mind what, or if, you are currently trying to achieve. Then you don't have to have a bad concience when you're not writing that essay while playing jenga at the hostel, because you know there's a better time for that in an environment that's perfectly tailored to the this objetive.   
+</p> 
+
+
+<h4>Was loneliness an issue?</h4>
+<p>
+For me not so much. Sightseeing and exploring i prefer with other people, so I tried so sign up for day tours or events directly at the hostel, that was the best way to connect with other travellers. Especially in hostels, there are a bunch of solo travellers that want to connect, so that's rarely an issue if you're confident enough to strike up a conversation. And even during time periods when I wasn't too sociable, being connected to friends and family over skype, whatsapp etc never really makes you feel alone. Technology ftw!
+</p>
+
+
+
+<h4>Some other points before I forget</h4>
+<ul>
+	<li>America was the bad guy in Vietnam war.</li>
+	<li>Plastic waste is a tangible issue (you literally touch it when swimming) in Bali. </li>
+	<li>If choice of bunk bed, always chose bottom.</li>
+	<li>Sharing a room with other people is not a problem.</li>
+	<li>Partying is not really fun for me anymore.</li>
+	<li>4 males can fit on one scooter.</li>
+	<li>Even quiet people want to talk, as long as one asks the right questions.</li>
+	<li>SE-Asia is super safe. Not one criminal incident in six months.</li>
+
 
 <figure class="figure">
 	<img src="/images/travel-summary/hanoi.jpg" class="img-thumbnail figure-img img-fluid rounded">
 	<figcaption class="figure-caption">
-		Hanoi, Vietnam. Look how lively.
+		Hanoi, Vietnam: Generally, streets in SE-Asia were full of people socializing, everytime of the day. Coming back home, I noticed how few people you see outside. Does money bring isolation?
 	<figcaption>
 </figure>
-
-<p>
-How many people are currently travelling or working remotely. That that's a life one can have and choose.
-</p>
-
-
-
-<h4>Some other things I learned</h4>
-<ul>
-	<li>America was the bad guy in Vietnam war.</li>
-	<li>Plastic waste is a real issue in Bali. Paddled out through dirty water in Canggu.</li>
-	<li>Top bunk bed is not better than bottom bed.</li>
-	<li>Sharing a room with other people is not problem.</li>
-	<li>Partying is really not fun for me anymore.</li>
-	<li>4 males can fit on one scooter</li>
-	<li>SE-Asia is extremely safe. Not one criminal incident in 6 months. Also never used a locker in the dorms (just a lock on my bag) and never had anything stolen.  
-</ul>
 
 
 <figure class="figure">
 	<img src="/images/travel-summary/sinag-hostel.jpg" class="img-thumbnail figure-img img-fluid rounded">
 	<figcaption class="figure-caption">
-		Sinag hostel in Siargao, Philipines. Amazing place.
+		The Secret Society, pssst! [<a href="https://goo.gl/maps/effeQNrSTf42">Sinag hostel<a>, Philipines].
 	<figcaption>
 </figure>
 
 
 
-<h4>Most experience of freedom and explorer mindset</h4>
-<p>
-Generally sitting on buses/trains/boats looking out the window and seeing the world pass by. It gave me a peculiar feeling of possibility. That I can go anywhere for a few bucks, do anything. This feeling of not beeing constrained. This urge that I'm obliged to use it and not let it go to waste.
-Sitting in Bus driving to Pai listening to <insert song here> looking out the window, felt freeing, because I didn't plan going there until bying a ticket on the spot.
-Walking/Jogging around in totally strange Phnom Phen, felt like in an amusement park (Monks just walking around).
-</p>
-
-
-
-<h4>Was loneliness an issue?</h4>
-<p>
-Not so much for me. Sightseeing/exploring I prefer to do with other people. Going to a restaurant I also have no problem going alone. Especially breakfast I actually prefer alone with a a book. BUT, the regular visits as well as great skype talks with friends and family always made me feel connected. Technology ftw!
-</p>
-
-
-<h4>What will I do differently next time?</h4>
-<p>
-I noticed that six months of pure travelling/sightseeing is quite long. 3 weeks every 6 months sight-seeing vacations are enough. Pick one country and go all-in, hostel, social, tours. The other time stay in a place with routine, and work efficiently on something (optimally this is my job) and do sports, e.g. surfing. Because pure travel can be exhausting and conversely, working efficiently and doing sports is difficult on the road. 
-</p>  
+ 
 
 
   `,
